d1e38e54de4449be5cc54d60320ed5f1f3d30cbc
Darina44 <johndoe@email.com>
2022-11-06 18:03:33 +0300

    event keyup deleted

--- function/index.js
+++ function/index.js
@@ -1,11 +1,11 @@
 const input = document.querySelector('input');
 const resultsort = document.getElementById('resultsort');
+const resultsum = document.getElementById('resultsum');
 
 let nums = [];
 
-document.addEventListener( 'keyup', sumInput => {
-    if( event.code === 'Enter' ) {
-    console.log('enter was pressed');
+input.addEventListener( 'change', () => {
+    if(input.value !== '' && !/[^0-9\.]/g.test(input.value)) {
     let number = input.value;
     nums.push(+number);
     input.value = '';
@@ -14,6 +14,7 @@ document.addEventListener( 'keyup', sumInput => {
 let button = document.getElementById("done");
 
 button.onclick = function () {
+    
     nums.sort((a, b) => a - b);
     resultsort.innerText = `Массив: ${nums}`;
 
